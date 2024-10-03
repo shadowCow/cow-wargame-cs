aa9d94b8bbcae4b8eda8a024067f1f81472ddeaf
shadowCow <shadowCow84@gmail.com>
2024-10-03 19:12:50 -0400

    add maui client.

---
+++ game-client/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace game_client;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ game-client/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace game_client;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
